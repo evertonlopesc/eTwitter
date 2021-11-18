b943c01d9e0c8e52550547e2e6d5d0931f418a06
evertonlopesc <everton.lopes@outlook.com>
2021-11-18 09:24:43 -0300

    Testing the user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :following, foreign_key: "follower_id", class_name: "Relationship" 
   has_many :notifications, dependent: :destroy
 
+  validates :username, presence: true, uniqueness: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @user = build(:user)
+  end
+
+  context 'when save new user' do
+    it 'with username' do
+      expect(@user).to be_valid
+    end
+
+    it 'without username' do
+      @user.username = ''
+
+      expect(@user).not_to be_valid
+    end
+
+    it 'without password' do
+      @user.password = ''
+
+      expect(@user).not_to be_valid
+    end
+
+    it 'password with less than six digits' do
+      @user.password = '1g23h'
+
+      expect(@user).not_to be_valid
+    end
+
+    it 'username not save duplicate' do
+      @user.username = 'Tarzan'
+      user = create(:user)
+      user.username = 'Tarzan'
+
+      expect(user.errors[:username]).to include("has already been taken")
+    end
+  end
 end
